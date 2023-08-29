3d1ba1bd0ebb46f71da4732af6efe809d146949f
Pedro Gabriel Freitas da Silva <142266064+KNZTlyon@users.noreply.github.com>
2023-08-28 21:42:09 -0400

    Add files via upload

---
+++ Atividade1/src/uniderp/loo/escola/dominio/Professor.java
@@ -0,0 +1,31 @@
+package uniderp.loo.escola.dominio;
+
+import java.time.LocalDate;
+
+public class Professor extends BasePessoa {
+
+    private String registro;
+    private LocalDate datavContratacao;
+    public String getRegistro() {
+        return registro;
+    }
+    public void setRegistro(String registro) {
+        this.registro = registro;
+    }
+    public LocalDate getDatavContratacao() {
+        return datavContratacao;
+    }
+    public void setDatavContratacao(LocalDate datavContratacao) {
+        this.datavContratacao = datavContratacao;
+    }
+    public Professor(int codigo, String nome, String endereco, String telefone, LocalDate data, String rg, String cpf,
+            LocalDate dataInsercao, String registro, LocalDate datavContratacao) {
+        super(codigo, nome, endereco, telefone, data, rg, cpf, dataInsercao);
+        this.registro = registro;
+        this.datavContratacao = datavContratacao;
+    }
+
+    public Professor(){
+
+    }
+}
